369cd149598a51171b97ee3a20c19a056f465aad
Csaba Valyi <csaba_valyi@epam.com>
2016-07-21 16:54:29 +0200

    Remove unused import in Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
